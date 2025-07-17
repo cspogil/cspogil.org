ad719e4a621ab17bb83d6099550e5373eab499dd
Chris Mayfield <mayfiecs@jmu.edu>
2025-07-17 11:30:48 -0400

    Add home link to 404 page as well

--- hooks/home.py
+++ hooks/home.py
@@ -1,27 +1,32 @@
 """Make the site_name link to the home page."""
 
+import os
 
-def on_post_page(output, page, config):
-    """Called after the template is rendered."""
 
-    # Get relative link to home page
-    home_path = "../" * page.url.count("/")
-    if home_path:
-        # Remove the trailing slash
-        home_path = home_path[:-1]
-    else:
-        # Special case: already home
-        home_path = "."
-
-    # Find and modify each instance
+def patch(output, config):
+    """Find and modify each site_name."""
     logo = config["theme"]["logo"]
     name = config["site_name"]
-    link = f'<a href="../../..">{name}</a>'
+    link = f'<a href="/">{name}</a>'
     beg = 0
     while (beg := output.find(logo, beg)) > -1:
         # Find site_name after the logo
         beg = output.index(name, beg)
         end = beg + len(name)
         output = output[:beg] + link + output[end:]
-
     return output
+
+
+def on_post_page(output, page, config):
+    """Called after the template is rendered."""
+    return patch(output, config)
+
+
+def on_post_build(config):
+    """Use this event to call post-build scripts."""
+    path = config["site_dir"] + os.sep + "404.html"
+    with open(path, "r") as file:
+        output = file.read()
+    output = patch(output, config)
+    with open(path, "w") as file:
+        file.write(output)
