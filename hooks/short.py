b0ccf401d2aa47a89b126506f3474034fbf104f3
Chris Mayfield <mayfiecs@jmu.edu>
2025-07-17 22:16:29 -0400

    Remove duplicate code in bibgen.py and short.py

--- hooks/short.py
+++ hooks/short.py
@@ -1,10 +1,9 @@
-"""Various shortcuts and find-and-replace patterns."""
+"""Find and replace short patterns and render BibTeX entries."""
 
 import bibtexparser
 import io
 import os
 import re
-import sys
 
 
 SUBS = [
@@ -25,38 +24,70 @@ def on_page_markdown(markdown, page, config, files):
         md_path = "docs/" + page.file.src_uri
         bib_path = md_path[:-2] + "bib"
         if os.path.exists(bib_path):
-
-            # Parse the bib file
-            library = bibtexparser.parse_file(bib_path)
-            n = len(library.failed_blocks)
-            if n > 0:
-                print(f"Error: {n} blocks failed to parse")
-                sys.exit(1)
-            n = len(library.entries)
-            if n != 1:
-                print(f"Error: Found {n} entries (should be 1)")
-                sys.exit(1)
-
-            # Generate the Markdown
+            entry = parse_entry(bib_path)
             out = io.StringIO()
-            out.write("\n## Metadata\n\n")
-            name = os.path.basename(bib_path)
-            down = '{download="' + name + '"}'
-            out.write(f"[:material-download: Download .bib file]({name}){down}\n\n")
-            out.write("Field | Value\n------|------\n")
-            entry = library.entries[0]
-            for fkey, field in entry.fields_dict.items():
-                out.write(f"{fkey} | ")
-                if fkey != "url":
-                    out.write(f"{field.value}\n")
-                else:
-                    # Abbreviate the URL to allow text wrapping
-                    url = field.value
-                    pos = url.index("/", 8)
-                    abbr = url[:pos+1] + "..."
-                    out.write(f"[{abbr}]({url})\n")
-
-            # Append at the bottom
+            write_entry(entry, out)
             markdown += out.getvalue()
 
     return markdown
+
+
+def parse_entry(path):
+    """Parse the given bib file and return the entry."""
+
+    # Abort if the file format is incorrect
+    library = bibtexparser.parse_file(path)
+    n = len(library.failed_blocks)
+    if n > 0:
+        raise ValueError(f"{n} blocks failed to parse")
+    n = len(library.entries)
+    if n != 1:
+        raise ValueError(f"{n} entries found (not 1)")
+    return library.entries[0]
+
+
+def write_entry(entry, out):
+    """Output the given entry in Markdown format."""
+
+    # Write the "Contents" section if present
+    field = entry.fields_dict.get("contents")
+    if field:
+
+        # Get the index after the left brace
+        pattern = r"^\s*contents\s*=\s*{"
+        match = re.search(pattern, entry.raw, re.MULTILINE)
+        col = len(match.group())
+
+        # Remove leading whitespace from each line
+        lines = field.value.splitlines()
+        for i in range(1, len(lines)):
+            line = lines[i]
+            beg = 0
+            while beg < len(line) and beg < col and line[beg] == " ":
+                beg += 1
+            lines[i] = line[beg:]
+
+        # The resulting lines are Markdown format
+        out.write("\n## Contents\n\n")
+        out.write("\n".join(lines) + "\n")
+
+    # Write section heading and download link
+    out.write("\n## Metadata\n\n")
+    name = entry.key + ".bib"
+    down = '{download="' + name + '"}'
+    out.write(f"[:material-download: Download .bib file]({name}){down}\n\n")
+
+    # Write field-value pairs in table format
+    out.write("Field | Value\n------|------\n")
+    for fkey, field in entry.fields_dict.items():
+        if fkey == "contents":
+            continue
+        out.write(f"{fkey} | ")
+        if fkey != "url":
+            out.write(f"{field.value}\n")
+        else:
+            # Abbreviate the URL to allow text wrapping
+            url = field.value
+            pos = url.index("/", 8)
+            abbr = url[:pos+1] + "..."
+            out.write(f"[{abbr}]({url})\n")
